5b54bf8fa44603d7dd919c04c23f5a0a9931a4fa
lukagobechia <lukagobechiaa@gmail.com>
2023-06-16 17:17:25 +0400

    added MainMenu(separate file)

---
+++ MENUSFML.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include "MainMenu.h"
+using namespace sf;
+
+//Render Windowsrc
+Vector2f viewSize(1000,720);
+VideoMode vm(viewSize.x,viewSize.y);
+RenderWindow MENU(vm, "Main Menu", Style::Default);
+
+MainMenu mainMenu(MENU.getSize().x,MENU.getSize().y);
+
+
+
+int main(){
+    
+//set background
+RectangleShape background;
+background.setSize(Vector2f(1000,720));
+Texture Maintexture;
+Maintexture.loadFromFile("Texture/bg.jpg");
+background.setTexture(&Maintexture);
+
+while(MENU.isOpen())
+{
+    Event event;
+    while(MENU.pollEvent(event)){
+        if(event.type == Event::Closed)
+        {
+            MENU.close();
+        }
+
+        if(event.type == Event::KeyReleased)
+        {
+            if(event.key.code == Keyboard::Up){
+                mainMenu.MoveUp();
+                break;
+            }
+            if (event.key.code == Keyboard::Down) {
+                mainMenu.MoveDown();
+                break;
+            }
+            if(event.key.code == Keyboard::Return){
+                RenderWindow Play(vm,"game_name");
+
+                int x = mainMenu.MainMenuPressed();
+
+                if(x == 0)
+                {
+                    while(Play.isOpen()){
+                        Event playevent;
+                        while (Play.pollEvent(playevent))
+                        {
+                           if(playevent.type == Event::Closed)
+                           {
+                                Play.close();
+                           }
+                           if(playevent.type == Event::KeyPressed)
+                           {
+                            if(playevent.key.code == Keyboard::Escape)
+                            {
+                                Play.close();
+                            }
+                               
+                           }                          
+                        }
+                        Play.clear();
+                        Play.display();
+                    }
+
+                }
+                if(x == 1){
+                    MENU.close();
+                    break;
+                }
+            } 
+        } 
+    }
+    MENU.clear();
+    MENU.draw(background);     
+    mainMenu.draw(MENU);
+    MENU.display();
+  }
+}
+
---
+++ MainMenu.cpp
@@ -0,0 +1,62 @@
+#include "MainMenu.h"
+using namespace sf;
+
+MainMenu::MainMenu(float width, float height)
+{
+    if(!font.loadFromFile("Fonts/Pixel_Art.ttf")){
+        std::cout << "No font is here";
+    }
+
+    // play
+    mainMenu[0].setFont(font);
+    mainMenu[0].setFillColor(Color::Black);
+    mainMenu[0].setString("START");
+    mainMenu[0].setCharacterSize(50);
+    mainMenu[0].setPosition(395,300);
+    // exit
+    mainMenu[1].setFont(font);
+    mainMenu[1].setFillColor(Color::Black);
+    mainMenu[1].setString("EXIT");
+    mainMenu[1].setCharacterSize(50);
+    mainMenu[1].setPosition(422,350);
+
+    MainMenuSelected = -1;
+}
+MainMenu::~MainMenu()
+{
+
+}
+
+// draw main menu
+void MainMenu::draw(RenderWindow& window){
+    for(int i = 0; i< Max_main_menu; i++){
+        window.draw(mainMenu[i]);
+    }
+}
+//Move Up
+void MainMenu::MoveUp()
+{
+    if(MainMenuSelected - 1 >= 0){
+        mainMenu[MainMenuSelected].setFillColor(Color::Black);
+       
+        MainMenuSelected --;
+        if(MainMenuSelected == 1){
+            MainMenuSelected ==  2;
+        }
+        mainMenu[MainMenuSelected].setFillColor(Color::Blue);  
+    }
+}
+//MoveUp
+void MainMenu::MoveDown()
+{
+    if(MainMenuSelected + 1 <= Max_main_menu){
+        mainMenu[MainMenuSelected].setFillColor(Color::Black);
+        MainMenuSelected++;
+        
+        if(MainMenuSelected == Max_main_menu){
+            MainMenuSelected = 0;
+        }
+        
+        mainMenu[MainMenuSelected].setFillColor(Color::Blue);
+    }
+}
